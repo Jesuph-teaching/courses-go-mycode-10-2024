a06a56a6479d707126b07270557acace41fc6578
Youcef Madadi <tahher@live.fr>
2025-01-16 15:39:48 +0100

    adding brands and location

--- practice/cars-project-ejs/index.js
+++ practice/cars-project-ejs/index.js
@@ -6,7 +6,38 @@ const PORT = 4545;
 app.set("view engine", "ejs");
 
 app.use("/public", express.static("./public"));
+
+const brands = [
+	{ image: "/public/images/ford.svg", title: "Ford" },
+	{ image: "/public/images/Tesla.svg", title: "Tesla" },
+	{ image: "/public/images/Toyota.svg", title: "Toyota" },
+	{ image: "/public/images/ford.svg", title: "Ford" },
+	{ image: "/public/images/Tesla.svg", title: "Tesla" },
+	{ image: "/public/images/Toyota.svg", title: "Toyota" },
+	{ image: "/public/images/ford.svg", title: "Ford" },
+	{ image: "/public/images/Tesla.svg", title: "Tesla" },
+	{ image: "/public/images/Toyota.svg", title: "Toyota" },
+	{ image: "/public/images/ford.svg", title: "Ford" },
+	{ image: "/public/images/Tesla.svg", title: "Tesla" },
+	{ image: "/public/images/Toyota.svg", title: "Toyota" },
+];
+const bodyTypes = [
+	{ image: "/public/images/crossover.svg", title: "Crossover" },
+	{ image: "/public/images/suv.svg", title: "SUV" },
+	{ image: "/public/images/wagon.svg", title: "Wagon" },
+	{ image: "/public/images/crossover.svg", title: "Crossover" },
+	{ image: "/public/images/suv.svg", title: "SUV" },
+	{ image: "/public/images/wagon.svg", title: "Wagon" },
+	{ image: "/public/images/crossover.svg", title: "Crossover" },
+	{ image: "/public/images/suv.svg", title: "SUV" },
+	{ image: "/public/images/wagon.svg", title: "Wagon" },
+	{ image: "/public/images/crossover.svg", title: "Crossover" },
+	{ image: "/public/images/suv.svg", title: "SUV" },
+	{ image: "/public/images/wagon.svg", title: "Wagon" },
+];
 app.get("/", (req, res) => {
+	res.locals.brands = brands;
+	res.locals.bodyTypes = bodyTypes;
 	res.render("home");
 });
 app.listen(PORT, () => {
