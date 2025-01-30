edf5f40b9860cee52a757dfb6b57fe4847c8a5fb
Youcef Madadi <tahher@live.fr>
2025-01-30 10:26:05 +0100

    challenge 1

--- 6.react-tests/effect-example/src/TestComponent.jsx
+++ 6.react-tests/effect-example/src/TestComponent.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function TestComponent() {
   const [counter, setCounter] = useState(0);
