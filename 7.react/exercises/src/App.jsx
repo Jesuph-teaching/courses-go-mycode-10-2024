2ad824333f49dfe3c94881aae6003735d275f3cf
Youcef Madadi <tahher@live.fr>
2025-01-30 14:45:28 +0100

    adding progress example

--- 7.react/exercises/src/App.jsx
+++ 7.react/exercises/src/App.jsx
@@ -1,9 +1,13 @@
+import ProgressTracker from "./exercises/ProgressTracker";
 import Stopwatch from "./exercises/Stopwatch";
+import Todo from "./exercises/Todo";
 
 function App() {
   return (
     <>
+      <ProgressTracker />
       <Stopwatch />
+      <Todo />
     </>
   );
 }
